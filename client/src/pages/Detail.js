4f03e2dce40949e819f3997d00e79b6c9cb04a66
Evan Arbour <evan.arbour@gmail.com>
2021-11-16 09:57:29 -0800

    installed react-redux and imported to Detail.js

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,7 +3,6 @@ import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -14,8 +13,13 @@ import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
+// import redux
+import { useSelector, useDispatch } from 'react-redux';
+
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
