59c3d41697567dcb9558f3b296221deb8f9d79db
Evan Arbour <evan.arbour@gmail.com>
2021-11-17 09:16:59 -0800

    commit to heroku

---
+++ client/build/precache-manifest.fd9db4352482d5c35d10fa483715e8b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f649f5f2220458ff809d46f663c00178",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ab89aa3a91e9b3f075c",
+    "url": "/static/css/main.6816d842.chunk.css"
+  },
+  {
+    "revision": "a651457359b77e7e3dd5",
+    "url": "/static/js/2.808e23ab.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.808e23ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab89aa3a91e9b3f075c",
+    "url": "/static/js/main.ecfc0824.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
